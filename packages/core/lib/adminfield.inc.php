69c720227584644a108d7ccd0e934c9d0f9480d4
luisgreg99 <luisram90_101@hotmail.com>
2018-03-08 08:50:01 -0800

    updated outputHierarchicalSelect to default existing classification/collection records

--- packages/core/lib/adminfield.inc.php
+++ packages/core/lib/adminfield.inc.php
@@ -376,6 +376,7 @@ abstract class Core_AdminField
       }
       else // Find out if an id has been set in the database
       {
+         $tempClassificationIdValue = NULL;
          foreach($this->FieldNames as $key => $FieldName)
          {
             $tempValue = $this->getFieldValue($FieldName);
@@ -384,6 +385,10 @@ abstract class Core_AdminField
             {
                $MasterFieldName = $FieldName;
                $MasterFieldValue = $tempValue;
+               if (strcmp($FieldName, "ClassificationID") == 0)
+               {
+                   $tempClassificationIdValue = $tempValue;
+               }
             }
          }
       }
@@ -446,6 +451,10 @@ abstract class Core_AdminField
          else if(count($this->FieldNames) > 1) // The field's class has no hierarchy
          {
             $tempTopObject = New $this->ClassNames[$key]($FieldValues[$key]);
+             if (isset($tempClassificationIdValue))
+             {
+                 $tempTopObject->ClassificationID = $tempClassificationIdValue;
+             }
             $tempTopObject->dbLoad();
             $TopObjects[$key] = $tempTopObject;
          }
